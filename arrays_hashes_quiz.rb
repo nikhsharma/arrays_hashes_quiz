1c415b057cb78bab3d1808ecd03c1369477c0357
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-03-28 17:07:31 +0100

    completed excercise C

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -185,7 +185,19 @@ united_kingdom.push({
   population: 1811000,
   capital: "Belfast"
 })
-p united_kingdom
 
 # 3. Use a loop to print the names of all the countries in the UK.
+
+for country in united_kingdom
+  p country[:name]
+end
+
 # 4. Use a loop to find the total population of the UK.
+
+total_population = 0
+
+for country in united_kingdom
+  total_population += country[:population]
+end
+
+p total_population
